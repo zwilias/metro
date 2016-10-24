e9c14f333a6e7fae9e5007a3354e3af6cab2d2dc
Van Peer Ilias <ilias.van.peer@telenetgroup.be>
2016-10-24 20:57:37 +0200

    Passthrough to children for MetroElementCollection

--- src/MetroElementCollection.js
+++ src/MetroElementCollection.js
@@ -1,3 +1,11 @@
+import MetroComponent from './MetroComponent';
+
 //@flow
-class MetroElementCollection {}
+class MetroElementCollection extends MetroComponent {
+    renderInto = (container) => {
+        this.props.children.forEach((child) => {
+            child.beforeMount().renderInto(container);
+        });
+    }
+}
 export default MetroElementCollection;
